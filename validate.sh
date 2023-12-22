fca7f1d127c98aa4880584e6b83c8eae040ee2c0
Sol-bobo <154569130+Sol-bobo@users.noreply.github.com>
2023-12-22 02:09:30 +0000

    Validate Bobo token.

--- validate.sh
+++ validate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 cue vet src/tokens/solana.tokenlist.json automerge/schema.cue -d '#Tokenlist'
+Bobo,BOBO,5EL7LTHh5SLMkxuEeCNKdgRaSMSsZJnk4K9Z5smtZo47,9,https://node1.irys.xyz/XsV7eHxNRbIggUieJTtTos60qKZ1Dw2MVSgnR_CwL6U,true
